4c6fdf1fd9d42b4317bd3c8ea1cb1702c349fd49
samuel-lema <ec2-user@ip-172-31-28-222.us-east-2.compute.internal>
2018-11-27 09:58:45 +0000

    Footer con miWIdget y Nav menu.

--- footer.php
+++ footer.php
@@ -21,24 +21,31 @@ if ( onepress_is_wc_active() ) {
     }
 }
 
+
+
 if ( ! $hide_footer ) {
     ?>
     <footer id="colophon" class="site-footer" role="contentinfo">
+
         <?php
 
+            dynamic_sidebar( 'funcion' );
             do_action('onepress_before_site_info');
+            
             $onepress_btt_disable = sanitize_text_field(get_theme_mod('onepress_btt_disable'));
 
         ?>
-
+            
         <div class="site-info">
             <div class="container">
                 <?php if ($onepress_btt_disable != '1') : ?>
                     <div class="btt">
                         <a class="back-to-top" href="#page" title="<?php echo esc_html__('Ir al Inicio (Arriba)', 'onepress') ?>"><i class="fa fa-angle-double-up wow flash" data-wow-duration="2s"></i></a>
                     </div>
+                    
                 <?php endif; ?>
                 <?php
+                    wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
                     echo("<h3 style='color: #FFF;'> Copyright © 2018 Game Motion</h3>");
                 ?>
             </div>
