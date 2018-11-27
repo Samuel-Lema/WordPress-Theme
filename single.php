fbe86063352489d3cdfe04f1f7f0822829f8910d
samuel-lema <ec2-user@ip-172-31-28-222.us-east-2.compute.internal>
2018-11-27 10:07:52 +0000

    Página de noticias (Single), modificado datos a mostrar igual que el loop en home.php

--- single.php
+++ single.php
@@ -1,19 +1,11 @@
 <?php
 /**
- * The template for displaying all single posts.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package OnePress
+ * Carga la página por defecto del post simple
  */
 
 get_header();
 $layout = onepress_get_layout();
 
-/**
- * @since 2.0.0
- * @see onepress_display_page_title
- */
 do_action( 'onepress_page_before_content' );
 ?>
 
@@ -28,13 +20,6 @@ do_action( 'onepress_page_before_content' );
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					/*if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;*/
-					?>
 
 				<?php endwhile; // End of the loop. ?>
 
